bdadeb2a1bb6b77edc5df20ccab2cd9f4f36fbdb
Jameson Holleran <holleranjameson34@gmail.com>
2026-07-17 02:18:34 -0400

    Add custom keybinds, Aetheria theme default, GRUB 1080p

--- install/config/theme.sh
+++ install/config/theme.sh
@@ -10,7 +10,8 @@ sudo mkdir -p /etc/chromium/policies/managed
 sudo chmod a+rw /etc/chromium/policies/managed
 
 # Set initial theme
-omarchy-theme-set "Tokyo Night"
+omarchy-theme-install https://github.com/JJDizz1L/aetheria.git
+omarchy-theme-set "aetheria"
 rm -rf ~/.config/chromium/SingletonLock # otherwise archiso will own the chromium singleton
 
 # Set specific app links for current theme
