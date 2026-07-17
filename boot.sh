1d9e1908cd27793ae54b98b32061ca55b1da6cec
Jameson Holleran <holleranjameson34@gmail.com>
2026-07-17 01:53:43 -0400

    Fix boot.sh: download-then-run instead of process substitution

--- boot.sh
+++ boot.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# Set install mode to online since boot.sh is used for curl installations
-export OMARCHY_ONLINE_INSTALL=true
+set -eEo pipefail
 
 ansi_art='                 ▄▄▄
  ▄█████▄    ▄███████████▄    ▄███████   ▄███████   ▄███████   ▄█   █▄    ▄█   █▄
@@ -17,15 +16,15 @@ ansi_art='                 ▄▄▄
 clear
 echo -e "\n$ansi_art\n"
 
-# Fork defaults: NetworkManager + GRUB + LiquidGlass Dark theme
+export OMARCHY_ONLINE_INSTALL=true
 export OMARCHY_MIRROR=stable
 echo 'Server = https://stable-mirror.omarchy.org/$repo/os/$arch' | sudo tee /etc/pacman.d/mirrorlist >/dev/null
 
 sudo pacman -Syu --noconfirm --needed git
 
-echo -e "\nCloning Omarchy fork from: https://github.com/Jammersmurph/omarchy-fork.git"
+echo -e "\nCloning Omarchy fork..."
 rm -rf ~/.local/share/omarchy/
 git clone https://github.com/Jammersmurph/omarchy-fork.git ~/.local/share/omarchy >/dev/null
 
 echo -e "\nInstallation starting..."
-source ~/.local/share/omarchy/install.sh
+bash ~/.local/share/omarchy/install.sh
